9ab8783604a4b93057aa3cbb4ac1eb93046803b2
explodoboy <fiffason@gmail.com>
2024-05-28 00:06:53 -0700

    added alt descriptions for under-construction page images

--- scripts/under_construction.js
+++ scripts/under_construction.js
@@ -26,27 +26,27 @@ window.onload = function() {
     {
         case 1:
             newImg.src = "images/construction_drive.png";
-            imgElement.alt = "replaceme";
+            imgElement.alt = "a construction worker driving away from a horde of giant green leeches with teeth. the worker is screaming in horror";
             break;
         case 2:
             newImg.src = "images/construction_fight.png";
-            imgElement.alt = "replaceme";
+            imgElement.alt = "a construction worker standing on top of an industrial vehicle, wielding a bladed powered weapon against a giant green leech with teeth";
 
             paragraph.innerText = "So get out of here, FLESHBAG. Or YOU—\nWait, what?";
             break;
         case 3:
             newImg.src = "images/construction_hide.png";
-            imgElement.alt = "replaceme";
+            imgElement.alt = "a construction worker hiding underneath a sheet of metal from a giant green leech with teeth, unaware that the leech has found him";
             break;
         case 4:
             newImg.src = "images/construction_pov.png";
-            imgElement.alt = "replaceme";
+            imgElement.alt = "a point-of-view image of a construction worker being attacked by a giant green leech with teeth.";
 
             paragraph.innerText += "\n\nIn fact, here is a visual guide.";
             break;
         case 5:
             newImg.src = "images/construction_run.png";
-            imgElement.alt = "replaceme";
+            imgElement.alt = "a construction worker in panic running away from a horde of giant green leeches with teeth";
             break;
         default:
             console.log("ERROR: Construction random number script gave a number not between 1 - 5." + random);
